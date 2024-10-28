6a47314b84814727bb34cff2338c5cef133eb6b3
choimingyu <alsrb1315093@gmail.com>
2024-10-28 15:25:30 +0900

    프로젝트 완료

--- app/src/main/java/com/example/textedu/MainActivity.kt
+++ app/src/main/java/com/example/textedu/MainActivity.kt
@@ -4,13 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.basicMarquee
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Cyan
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.textedu.ui.theme.TextEduTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +37,124 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TextEduTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MarqueeText("Please subscribe and like")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun BasicText(message: String){
+    TextEduTheme {
+        Text(
+            text = message,
+            fontSize = 32.sp,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun ShadowText(message: String){
+    val offset = Offset(5.0f, 10.0f)
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = message,
+        fontSize = 24.sp,
+        style = TextStyle(
+            shadow = Shadow(
+                color = Color.Blue,
+                offset = offset,
+                blurRadius = 3f //흐리게 처리되는 정도
+            )
+        )
     )
+}
+
+@Composable
+fun MultipleText(message: String){
+    Text(
+        buildAnnotatedString {
+            withStyle(
+                style = SpanStyle(color = Color.Blue)
+            ){
+                append("H")
+            }
+            append("ello ")
+            withStyle(
+                style = SpanStyle(fontWeight = FontWeight.Bold, color = Color.Red)
+            ){
+                append("W")
+            }
+            append("orld")
+        }
+    )
+}
+
+@Composable
+fun BrushText(message: String){
+    Text(
+        text = message,
+        style = TextStyle(
+            fontSize = 24.sp,
+            brush = Brush.linearGradient(
+                colors = listOf(Color.Cyan, Color.Yellow, Color.DarkGray),
+            ),
+            alpha = .5f
+        )
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun MarqueeText(message: String){
+    Column (
+        modifier = Modifier.width(200.dp)
+    ){
+        Text(
+            text = message,
+            fontSize = 24.sp,
+            modifier = Modifier.basicMarquee()
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BasicTextPreview() {
     TextEduTheme {
-        Greeting("Android")
+        BasicText("Hello World")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShadowTextPreview() {
+    TextEduTheme {
+        ShadowText("Hello World")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MultipleTextPreview() {
+    TextEduTheme {
+        MultipleText("Hello World")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BrushTextPreview() {
+    TextEduTheme {
+        BrushText("Hello World")
+    }
+}
+
+@Preview
+@Composable
+fun MarqueePreview(){
+    TextEduTheme {
+        MarqueeText("Please subscribe and like")
     }
 }
